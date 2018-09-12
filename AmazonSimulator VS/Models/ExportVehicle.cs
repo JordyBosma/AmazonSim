34a172c013718ec44162dbeacbcbf92f2acf76c8
Pugnar <32259799+SwenvdWijngaard@users.noreply.github.com>
2018-09-12 16:06:24 +0200

    Parent Object3D class + child classes made

---
+++ AmazonSimulator VS/Models/ExportVehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ExportVehicle : Object3D
+    {
+        public ExportVehicle(double x, double y, double z, double rotationX, double rotationY, double rotationZ) : base(x, y, z, rotationX, rotationY, rotationZ, "ExportVehicle")
+        {
+
+        }
+    }
+}
