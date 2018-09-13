473742a65e267cc86d7fece0f1e5dcbd08e557f4
Pugnar <32259799+SwenvdWijngaard@users.noreply.github.com>
2018-09-13 14:30:09 +0200

    Logic Controller + multiple robot on load

---
+++ AmazonSimulator VS/Controllers/LogicController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    public static class LogicController
+    {
+        public static void ControllerLogic()
+        {
+
+        }
+    }
+}
--- AmazonSimulator VS/Models/World.cs
+++ AmazonSimulator VS/Models/World.cs
@@ -7,8 +7,23 @@ namespace Models {
     public class World : Model, IUpdatable
     {
         public World() {
-            Object3D r = CreateRobot(0,0,0);
-            r.Move(4.6, 0, 13);
+            Object3D a = CreateRobot(4.6, 0, 13);
+            Object3D b = CreateRobot(5.6, 0, 13);
+            Object3D c = CreateRobot(6.6, 0, 13);
+            Object3D d = CreateRobot(7.6, 0, 13);
+            Object3D e = CreateRobot(8.6, 0, 13);
+            Object3D f = CreateRobot(9.6, 0, 13);
+
+            //r.Move(4.6, 0, 13);
+        }
+
+        public void MoveRobot(double x, double z)
+        {
+            foreach (Object3D r in worldObjects)
+            {
+                if (r.type == "Robot")
+                r.Move(x, 0, z);
+            }
         }
 
         private Object3D CreateRobot(double x, double y, double z) {
